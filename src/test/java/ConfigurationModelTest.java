5f1483c100061e9207edb7ed8a4d067b91085d41
Jhon Jairo Alvarez Londoño <jjalvarezl@unicauca.edu.co>
2018-03-03 19:48:41 -0500

    updating jacoco reports, it does not worked unless it has a test class to execution

---
+++ src/test/java/ConfigurationModelTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConfigurationModelTest extends Assert {
+
+    @Test
+    public void test1(){
+        assertEquals(2,2);
+    }
+}
