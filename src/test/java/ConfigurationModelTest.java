4f303df6012bf0b540d7c4039872d7166bdc8da4
Jhon Jairo Alvarez Londoño <jjalvarezl@unicauca.edu.co>
2018-03-06 14:46:34 -0500

    Adding better tests

--- src/test/java/ConfigurationModelTest.java
+++ src/test/java/ConfigurationModelTest.java
@@ -1,11 +1,16 @@
 import org.junit.Assert;
 import org.junit.Test;
+import utils.Language;
+import views.LanguageChooser.LanguageChooserForm;
 import views.LanguageChooser.LanguageChooserView;
 
 public class ConfigurationModelTest extends Assert {
 
     @Test
     public void test1(){
-        assertEquals(LanguageChooserView.getInstance(),LanguageChooserView.getInstance());
+        Language.setResource(LanguageChooserForm.class.getName());
+        Language.setLanguage("en", "US");
+        System.out.println(Language.getResource().getString("FormTitle"));
+        assertTrue(Language.getResource().getString("FormTitle").equals("Language"));
     }
 }
